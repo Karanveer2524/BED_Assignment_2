ecd9a194dd60feac97db8c705401d460f5ebbb9d
Karanveer Singh <karanveersingh7@rrc.ca>
2025-03-21 21:10:07 -0500

    imported .env to firebase

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,11 +1,15 @@
 import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
 import { getFirestore, Firestore } from "firebase-admin/firestore";
-import * as serviceAccount from "../test-project-aadf9-firebase-adminsdk-fbsvc-d1e752358a.json"
+import * as dotenv from "dotenv";
 
-// Initialize the Firebase app with the service account credentials
-initializeApp({
-    credential: cert(serviceAccount as ServiceAccount),
-});
+// Load environment variables
+dotenv.config();
+
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'), // Handle newlines in private key
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+} as ServiceAccount;
 
 // Get a reference to the Firestore service
 const db: Firestore = getFirestore();
